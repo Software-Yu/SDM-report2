cfadf6d5b67a6a706c76d4222bb090f58dc1e121
Software-Yu <matsumoto.yu.sa@tut.jp>
2025-01-29 15:15:07 +0900

    test_cases add1
    テストケースの追加一回目

--- test_cases.py
+++ test_cases.py
@@ -18,4 +18,18 @@ class TestCalc (unittest.TestCase):
 
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
+        
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(1000,999))
+        
+        def test_sample6 (self):
+                self.assertEqual (999, calc(1,999))
+                
+        def test_sample7 (self):
+                self.assertEqual (998001, calc(999,999))   
 
+        def test_sample8 (self):
+                self.assertEqual (999, calc(999,1))  
+        
+        def test_sample9 (self):
+                self.assertEqual (-1, calc(1,"a"))  
